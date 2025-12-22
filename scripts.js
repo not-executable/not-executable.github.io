d6e48ed5930e2a8941f07ba5da6b75dd62fa5283
not-executable <125146946+not-executable@users.noreply.github.com>
2025-12-22 00:09:29 -0500

    localstorage

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const aishite = document.getElementById("amelove")
 const display = document.getElementById("love")
 function loveAme() {
    display.textContent = lainlove;
+   localStorage.getItem('total', lainlove)
 }
 
 aishite.addEventListener('click', () => {
